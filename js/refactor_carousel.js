b2736ab7514a155e919f188cf45c31686f4fea8a
Dat <dat.nguyen@journeyh.io>
2020-06-15 08:39:47 +0700

    refactor some code

--- js/refactor_carousel.js
+++ js/refactor_carousel.js
@@ -2,18 +2,18 @@ class Carousel {
   constructor(params) {
     this.carouselContainer = params.carouselContainer;
     this.controler = params.controler;
-    this.arrayItems = [...params.arrayItems];
-    this.primaryArrItems = [...params.arrayItems];
+    this.ItemsCarousel = [...params.arrayItems];
+    this.originalArrItems = [...params.arrayItems];
     this.indexOfFirstItem = 0;
-    this.isDraggable = true;
-    this.arrVisibleItems = [null, null, null, null, null, null];
+    this.canDrag = true;
+    this.arrVisibleItems = [];
 
-    this.isControlClickable = true;
+    this.canClickControl = true;
     this.setUpEvent()
   }
 
   setVisibleItemsCarousel() {
-    const lengthArrItems = this.arrayItems.length;
+    const lengthArrItems = this.ItemsCarousel.length;
     let indexItemInArrayItems = this.indexOfFirstItem;
 
     for (let i = 0; i < 6; i++) {
@@ -24,29 +24,33 @@ class Carousel {
         indexItemInArrayItems = 0;
       }
 
-      this.arrVisibleItems[i] = this.arrayItems[indexItemInArrayItems]
-      this.replaceClass(i)
+      this.arrVisibleItems[i] = this.ItemsCarousel[indexItemInArrayItems];
+      this.replaceClass(i);
       indexItemInArrayItems++;
     }
   }
 
   clearClass(i) {
-    this.arrVisibleItems[i].className = this.arrVisibleItems[i].className.replace(" zero", "").replace(" first", "").replace(" second", "").replace(" third", "").replace(" forth", "").replace(" fifth", "")
+    this.arrVisibleItems[i].className = this.arrVisibleItems[i].className.replace(" zero", "")
+    .replace(" first", "").replace(" second", "").replace(" third", "")
+    .replace(" forth", "").replace(" fifth", "")
+  }
+
+  setArrClass(len){
+    if (len >= 6) {
+      return [" first", " second", " third", " forth", " fifth", " zero"]
+    } else if (len == 3) {
+      return [" second", " third", " forth"]
+    } else if (len == 2) {
+      return [" third", " forth"]
+    } else if (len == 1) {
+      return [" third"]
+    }
   }
 
   addClass(i) {
-    let len = this.arrayItems.length;
-    let arrClass = [];
-
-    if (len >= 6) {
-      arrClass = [" first", " second", " third", " forth", " fifth", " zero"]
-    } else if (len == 3) {
-      arrClass = [" second", " third", " forth"]
-    } else if (len == 2) {
-      arrClass = [" third", " forth"]
-    } else if (len == 1) {
-      arrClass = [" third"]
-    }
+    let len = this.ItemsCarousel.length;
+    let arrClass = this.setArrClass(len)
 
     this.arrVisibleItems[i].className += arrClass[i]
   }
@@ -57,10 +61,10 @@ class Carousel {
   }
 
   moveBackward() {
-    if (this.isDraggable) {
+    if (this.canDrag) {
       this.indexOfFirstItem++;
 
-      if (this.indexOfFirstItem >= this.arrayItems.length)
+      if (this.indexOfFirstItem >= this.ItemsCarousel.length)
         this.indexOfFirstItem = 0;
 
       this.setVisibleItemsCarousel()
@@ -68,16 +72,15 @@ class Carousel {
   }
 
   moveForward() {
-    if (this.isDraggable) {
+    if (this.canDrag) {
       this.indexOfFirstItem--;
 
       if (this.indexOfFirstItem < 0)
-        this.indexOfFirstItem = this.arrayItems.length - 1
+        this.indexOfFirstItem = this.ItemsCarousel.length - 1
 
       this.setVisibleItemsCarousel()
     }
   }
-
 
   createDivHaveClassItem() {
     let newDiv = document.createElement("div");
@@ -87,8 +90,8 @@ class Carousel {
     return newDiv
   }
 
-  isDraggableFalse() {
-    let arrLength = this.arrayItems.length
+  canDragFalse() {
+    let arrLength = this.ItemsCarousel.length
     let WindowWidth = window.innerWidth
 
     const WidthMedium = 1024
@@ -103,44 +106,31 @@ class Carousel {
   }
 
   setUpItemFixed() {
-    let arrLength = this.arrayItems.length
-    let arrClass = []
-    if (arrLength == 3) {
-      arrClass = [" second", " third", " forth"]
-      // this.arrayItems[0].className += " second"
-      // this.arrayItems[1].className += " third"
-      // this.arrayItems[2].className += " forth"
-    } else if (arrLength == 2) {
-      arrClass = [" third", " forth"]
-      // this.arrayItems[0].className += " third"
-      // this.arrayItems[1].className += " forth"
-    } else if (arrLength == 1) {
-      arrClass = [" third"]
-      // this.arrayItems[0].className += " third"
-    }
+    let arrLength = this.ItemsCarousel.length
+    let arrClass = this.setArrClass(len)
 
     for (let i = 0; i < arrLength; i++) {
-      this.arrayItems[i].className += arrClass[i]
+      this.ItemsCarousel[i].className += arrClass[i]
     }
   }
 
   cloneArrayItems(arrLength) {
     //double the items until the array has the length which is more than 6
-    while (this.arrayItems.length < 6) {
+    while (this.ItemsCarousel.length < 6) {
       for (let i = 0; i < arrLength; i++) {
         let child = this.createDivHaveClassItem();
-        child.innerHTML = this.arrayItems[i].innerHTML
-        this.arrayItems.push(child)
+        child.innerHTML = this.ItemsCarousel[i].innerHTML
+        this.ItemsCarousel.push(child)
         this.carouselContainer.appendChild(child)
       }
     }
   }
 
   setUpDraggable() {
-    let arrLength = this.arrayItems.length
+    let arrLength = this.ItemsCarousel.length
 
-    if (this.isDraggableFalse()) {
-      this.isDraggable = false;
+    if (this.canDragFalse()) {
+      this.canDrag = false;
       this.setUpItemFixed()
     } else {
       if (arrLength < 6) {
@@ -153,22 +143,22 @@ class Carousel {
     this.indexOfFirstItem = 0;
     this.carouselContainer.innerHTML = "";
     console.log("carousel clear: " + this.carouselContainer.innerHTML)
-    this.arrayItems = [...this.primaryArrItems]
+    this.ItemsCarousel = [...this.originalArrItems]
 
-    for (let k = 0; k < this.arrayItems.length; k++) {
-      this.carouselContainer.appendChild(this.arrayItems[k])
+    for (let k = 0; k < this.ItemsCarousel.length; k++) {
+      this.carouselContainer.appendChild(this.ItemsCarousel[k])
     }
-    if (this.isDraggableFalse()) {
+    if (this.canDragFalse()) {
       //Not draggable
       //->set positon each card, hide controler
-      this.isDraggable = false
+      this.canDrag = false
       this.controler.style.display = "none"
       this.setVisibleItemsCarousel()
     } else {
-      this.isDraggable = true
+      this.canDrag = true
       // this.controler.style.display = "initial"
       //draggable
-      let arrLength = this.arrayItems.length
+      let arrLength = this.ItemsCarousel.length
       if (arrLength < 6) {
         this.cloneArrayItems(arrLength)
       }
@@ -179,18 +169,18 @@ class Carousel {
 
   preventClickingTooMuch(){
     setTimeout(() => {
-      this.isControlClickable = true
+      this.canClickControl = true
     }, 500)
   }
 
   setUpControler(){
-    if (!this.isDraggable) {
+    if (!this.canDrag) {
       this.controler.style.display = "none"
     } else {
       this.controler.addEventListener('click', () => {
-        if (this.isControlClickable && this.isDraggable) {
+        if (this.canClickControl && this.canDrag) {
           this.moveForward()
-          this.isControlClickable = false
+          this.canClickControl = false
 
           this.preventClickingTooMuch()
         }
@@ -208,14 +198,14 @@ class Carousel {
     let touchstartX = pairCoordinates.touchstartX
 
     touchendX = event.pageX;
-      console.log("isDraggable: ", this.isDraggable)
-      if (this.isControlClickable && this.isDraggable) {
+      console.log("canDrag: ", this.canDrag)
+      if (this.canClickControl && this.canDrag) {
         if (touchendX <= touchstartX) {
           this.moveBackward()
         } else if (touchendX > touchstartX) {
           this.moveForward()
         }
-        this.isControlClickable = false
+        this.canClickControl = false
 
         this.preventClickingTooMuch()
       }
